4b099e3c9b0586cf163bbb18be4c34ae697843f1
U2FsdGVkX1 <U2FsdGVkX1@gmail.com>
2021-05-14 16:55:42 +0800

    add bootlocal.sh

---
+++ bootlocal.sh
@@ -0,0 +1,57 @@
+zcat ../corepure64.gz | cpio -i -H newc -d
+rm -rf tmp.cpio.gz; find ./* | cpio -H newc -o > tmp.cpio; gzip -f tmp.cpio
+
+network="eth0"
+hostip=
+netmask=
+
+install() {
+    su tc -c "tce-load -wi $@"
+}
+
+parse_parameter() {
+    for x in $(cat /proc/cmdline); do
+        key=`echo $x | cut -d'=' -f1`
+        value=`echo $x | cut -d'=' -f2`
+        case "$key" in
+            password)
+            echo "tc:$value" | chpasswd
+            ;;
+            hostip)
+            hostip=$value
+            ;;
+            netmask)
+            netmask=$value
+            ;;
+            gateway)
+            route add default gw $value $network
+            ;;
+            nameserver)
+            echo "nameserver $value" > /etc/resolv.conf
+            ;;
+        esac
+    done
+}
+
+configure_network() {
+    if [ -n "$hostip" ]; then
+        ifconfig $network $hostip netmask $netmask
+        ifconfig $network up
+    fi
+}
+
+install_openssh() {
+    install openssh
+    cp /usr/local/etc/ssh/sshd_config.orig /usr/local/etc/ssh/sshd_config
+}
+
+install_tools() {
+    install e2fsprogs
+    install util-linux
+}
+
+parse_parameter
+configure_network
+install_tools
+install_openssh
+/usr/local/etc/init.d/openssh start
