53b6bc149ba930323f0e20f36895f6549356ada5
U2FsdGVkX1 <U2FsdGVkX1@gmail.com>
2021-05-14 20:13:23 +0800

    LiPE's Birthday!

---
+++ LiPE.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+BOOTLOCAL="https://raw.githubusercontent.com/U2FsdGVkX1/LiPE/main/bootlocal.sh"
+COREPURE="http://www.tinycorelinux.net/12.x/x86_64/release/CorePure64-12.0.iso"
+FILENAME=`basename $COREPURE`
+
+[ $EUID -ne 0 ] && echo "This script must be run as root" && exit 1
+
+if [ -e "/usr/bin/apt-get" ]; then
+    apt-get update
+elif [ -e "/usr/bin/yum" ]; then
+    yum update
+elif [ -e "/usr/bin/pacman" ]; then
+    pacman -Syy --noconfirm wget gzip cpio
+fi
+
+wget $COREPURE && wget $COREPURE.md5.txt && wget $BOOTLOCAL
+md5sum -c $FILENAME.md5.txt
+[ $? != 0 ] && echo "Download failed!" && exit 1
+
+mkdir TinyCore
+mount -o ro CorePure64-12.0.iso TinyCore
+[ $? != 0 ] && echo "Mount failed!" && exit 1
+
+cp TinyCore/boot/vmlinuz64 vmlinuz-lipe && cp TinyCore/boot/corepure64.gz initrd-lipe.gz
+umount TinyCore
+zcat initrd-lipe.gz | cpio -iD TinyCore -H newc
+[ $? != 0 ] && echo "Extracting failed!" && exit 1
+
+chmod +x bootlocal.sh
+mv bootlocal.sh TinyCore/opt
+cd TinyCore
+find . | cpio -o -H newc | gzip > ../initrd-lipe.gz
+[ $? != 0 ] && echo "Archiving failed!" && exit 1
+
+cd ..
+rm -rf TinyCore $FILENAME $FILENAME.md5.txt
+mv vmlinuz-lipe initrd-lipe.gz /boot
+if [ -d /boot/grub ]; then
+    grubreboot=grub-reboot
+    cfg=/boot/grub/custom.cfg
+elif [ -d /boot/grub2 ]; then
+    grubreboot=grub2-reboot
+    cfg=/boot/grub2/custom.cfg
+else
+    echo "Unable to find the GRUB directory!"
+    exit 1
+fi
+
+dev=`df /boot | tail -1 | cut -d' ' -f1`
+uuid=`blkid -s UUID -o value $dev`
+[ -z "$uuid" ] && echo "Unable to find the boot partition!" && exit 1
+echo "menuentry 'LiPE' {
+  set root='hd0,gpt2'
+  search --no-floppy --fs-uuid --set=root $uuid
+  linux /boot/vmlinuz-lipe password=12345678
+  initrd /boot/initrd-lipe.gz
+}
+" > $cfg
+$grubreboot LiPE
+reboot
